8c18fdf2bbd4b76c4441129750df95e5b40251ba
Joining7943 <joining@posteo.de>
2023-01-30 10:30:33 +0100

    Remove dummy code from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,3 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
