db0caf3975e98d85d53951520cdffcb5099208e7
Joining7943 <joining@posteo.de>
2023-01-30 10:05:02 +0100

    Add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2023 Joining7943 <joining@posteo.de>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
