d8c8a0a84e7e136d294fa48bcb1812fc529d3946
Joining7943 <joining@posteo.de>
2023-02-13 17:51:59 +0100

    fuzz: Fix false positives in case of exponent overflow errors

--- fuzz/fuzz_targets/f64_like_input.rs
+++ fuzz/fuzz_targets/f64_like_input.rs
@@ -2,8 +2,37 @@
 
 use std::num::IntErrorKind;
 
-use fundu::DurationParser;
+use fundu::{DurationParser, ParseError};
 use libfuzzer_sys::fuzz_target;
+
+fn check_exponent_overflow(input: &str, error: ParseError) {
+    match input.find(|c: char| c.eq_ignore_ascii_case(&'e')) {
+        Some(index) => match input.get(index + 1..) {
+            Some(exponent) => {
+                match exponent.parse::<i16>() {
+                    Ok(e) if (-1022..=1023).contains(&e) => panic!(
+                        "Exponent overflow error: Exponent was in valid range: input: {input}, error: {error:?}"
+                    ),
+                    Ok(_) => {
+                        // The overflow error is correctly returned by the parser
+                    }
+                    Err(error) => match error.kind() {
+                        IntErrorKind::PosOverflow | IntErrorKind::NegOverflow => {
+                            // The overflow error is correctly returned by the parser
+                        }
+                        kind => panic!(
+                            "Exponent overflow error: Should not be an Overflow error: {input}, error: {error:?}, kind: {kind:?}"
+                        ),
+                    }
+                }
+            }
+            None => panic!("Exponent overflow error: No number: input: {input}, error: {error:?}"),
+        },
+        None => {
+            panic!("Exponent overflow error: No exponent: input: {input}, error: {error:?}");
+        }
+    }
+}
 
 fuzz_target!(|data: &[u8]| {
     let mut parser = DurationParser::without_time_units();
@@ -11,10 +40,17 @@ fuzz_target!(|data: &[u8]| {
         if let Ok(parsed) = string.parse::<f64>() {
             if !parsed.is_nan() && parsed.is_sign_negative() && parsed != 0f64 {
                 match &parsed.abs().partial_cmp(&0.000000000000000001f64) {
-                    // Every negative number x with abs(x) < 1e-18 resolves to a zero duration
+                    // Every negative number x with abs(x) < 1e-18 resolves to a zero duration but
+                    // in case of an exponent overflow the error is returned.
                     Some(std::cmp::Ordering::Less) => {
                         if let Err(error) = parser.parse(string) {
-                            panic!("Expected no error: input: {string}, error: {error:?}");
+                            match error {
+                                ParseError::NegativeExponentOverflow
+                                | ParseError::PositiveExponentOverflow => {
+                                    check_exponent_overflow(string, error)
+                                }
+                                _ => panic!("Expected no error: input: {string}, error: {error:?}"),
+                            }
                         }
                     }
                     Some(std::cmp::Ordering::Equal) => {
@@ -25,7 +61,7 @@ fuzz_target!(|data: &[u8]| {
                         // suit for fuzzy testing here and has to be tested separately in a unit or
                         // integration test.
                     }
-                    // Every negative number x with abs(x) >= 1e-18 should be detected as negative
+                    // Negative numbers x with abs(x) >= 1e-18 should be detected as negative
                     // number and fundu returns an error
                     Some(std::cmp::Ordering::Greater) | None => {
                         if let Ok(duration) = parser.parse(string) {
@@ -43,40 +79,12 @@ fuzz_target!(|data: &[u8]| {
                     );
                 }
             // Everything else should be parsable by fundu besides some special handling of the exponent
-            // and the Overflow error
+            // and the overflow errors
             } else if let Err(error) = parser.parse(&format!("{parsed}")) {
                 match error {
-                    fundu::ParseError::Overflow => {
-                        match string.find(|c: char| c.eq_ignore_ascii_case(&'e')) {
-                            Some(index) => match string.get(index + 1..) {
-                                Some(exponent) => {
-                                    match exponent.parse::<i16>() {
-                                        Ok(e) if (-1022..=1023).contains(&e) => panic!(
-                                            "Overflow error: Exponent was in valid range: input: {string}, error: {error:?}"
-                                        ),
-                                        Ok(_) => {
-                                            // The overflow error is correctly returned by the parser
-                                        }
-                                        Err(error) => match error.kind() {
-                                            IntErrorKind::PosOverflow | IntErrorKind::NegOverflow => {
-                                                // The overflow error is correctly returned by the parser
-                                            }
-                                            kind => panic!(
-                                                "Overflow error: Should not be an Overflow error: {string}, error: {error:?}, kind: {kind:?}"
-                                            ),
-                                        }
-                                    }
-                                }
-                                None => panic!(
-                                    "Overflow error: No number: input: {string}, error: {error:?}"
-                                ),
-                            },
-                            None => {
-                                panic!(
-                                    "Overflow error: No exponent: input: {string}, error: {error:?}"
-                                );
-                            }
-                        }
+                    fundu::ParseError::NegativeExponentOverflow
+                    | fundu::ParseError::PositiveExponentOverflow => {
+                        check_exponent_overflow(string, error)
                     }
                     _ => {
                         panic!("Expected no error: input: {string}, error: {error:?}");
