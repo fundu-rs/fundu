a6a50d06a97f288e5d54f3dc3caae24d0ec24152
Joining7943 <joining@posteo.de>
2023-03-05 19:15:46 +0100

    bench: Improve iai benchmarks: No inlining and move references to own benchmark

--- benches/iai_bench_reference.rs
+++ benches/iai_bench_reference.rs
@@ -3,8 +3,8 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-use fundu::DurationParser;
 use iai::black_box;
+use std::time::Duration;
 
 const LARGE_INPUT: &str =
     "11111111111111111111111111111111111111111111111111111111111111111111111111111111\
@@ -36,28 +36,13 @@ const LARGE_INPUT: &str =
 const SMALL_INPUT: &str = "1";
 
 #[inline(never)]
-fn small_default_time_units() {
-    let _ = DurationParser::new().parse(black_box(SMALL_INPUT));
+fn small_reference() {
+    let _ = Duration::from_secs_f64(black_box(SMALL_INPUT).parse().unwrap());
 }
 
 #[inline(never)]
-fn small_without_time_units() {
-    let _ = DurationParser::without_time_units().parse(black_box(SMALL_INPUT));
+fn large_reference() {
+    let _ = Duration::from_secs_f64(black_box(LARGE_INPUT).parse().unwrap());
 }
 
-#[inline(never)]
-fn large_default_time_units() {
-    let _ = DurationParser::new().parse(black_box(LARGE_INPUT));
-}
-
-#[inline(never)]
-fn large_without_time_units() {
-    let _ = DurationParser::without_time_units().parse(black_box(LARGE_INPUT));
-}
-
-iai::main!(
-    small_default_time_units,
-    small_without_time_units,
-    large_default_time_units,
-    large_without_time_units,
-);
+iai::main!(small_reference, large_reference);
